866fd6a00444fb8dee9ad1fbdf19ed56d20729ce
Mathis Klooß <mathis@klooss.net>
2014-02-15 18:05:19 +0100

    rename performance to germansetup

--- app/code/community/Loewenstark/GermanSetup/Helper/Catalog/Product/Configuration.php
+++ app/code/community/Loewenstark/GermanSetup/Helper/Catalog/Product/Configuration.php
@@ -1,14 +1,14 @@
 <?php
 /**
-  * Loewenstark_Performance
+  * Loewenstark_GermanSetup
   *
   * @category  Loewenstark
-  * @package   Loewenstark_Performance
+  * @package   Loewenstark_GermanSetup
   * @author    Mathis Klooss <m.klooss@loewenstark.com>
   * @copyright 2013 Loewenstark Web-Solution GmbH (http://www.mage-profis.de/). All rights served.
-  * @license   https://github.com/mklooss/Loewenstark_Performance/blob/master/README.md
+  * @license   https://github.com/mklooss/Loewenstark_GermanSetup/blob/master/README.md
   */
-class Loewenstark_Performance_Helper_Catalog_Product_Configuration
+class Loewenstark_GermanSetup_Helper_Catalog_Product_Configuration
 extends FireGento_GermanSetup_Helper_Catalog_Product_Configuration
 {
     /**
---
+++ app/code/community/Loewenstark/GermanSetup/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+  * Loewenstark_GermanSetup
+  *
+  * @category  Loewenstark
+  * @package   Loewenstark_GermanSetup
+  * @author    Mathis Klooss <m.klooss@loewenstark.com>
+  * @copyright 2013 Loewenstark Web-Solution GmbH (http://www.mage-profis.de/). All rights served.
+  * @license   https://github.com/mklooss/Loewenstark_GermanSetup/blob/master/README.md
+  */
+class Loewenstark_GermanSetup_Helper_Data
+extends Mage_Core_Helper_Abstract
+{
+}
